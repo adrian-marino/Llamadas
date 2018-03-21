24eff1bfb8d3ba3834114d66c04091407f6919ae
Adrian Marino <adrian.marino@restorando.com>
2018-03-21 14:50:19 -0300

    format and append junit 4.x package imports to use annotations

--- src/test/java/com/prueba/DispatcherTest.java
+++ src/test/java/com/prueba/DispatcherTest.java
@@ -1,71 +1,67 @@
 package com.prueba;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
-import org.junit.Assert;
+import junit.framework.Assert;
 import org.junit.Test;
 
-public class DispatcherTest {
-	
-	private Dispatcher dispatcher = new Dispatcher();
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
 
-	/***
-	 * Prueba Unitaria para cumplir con el requerimiento
-	 * 		-> Procesar 10 llamadas al mismo tiempo
-	 * 
-	 * @throws InterruptedException
-	 * @throws ExecutionException
-	 */
-	@Test
-	public void dispatchCallDiezLlamadas() throws InterruptedException, ExecutionException {
-		List<Callable<Boolean>> tasks = Collections.nCopies(10, calleable());
-		ExecutorService executorService = Executors.newFixedThreadPool(10);
-		List<Future<Boolean>> llamadaEjecutas = executorService.invokeAll(tasks);
-		Thread.sleep(15000);
-		Assert.assertEquals(10, llamadaEjecutas.size());
-	}
-	
-	private Callable<Boolean> calleable(){
-		return new Callable<Boolean>() {
-			@Override
-			public Boolean call() throws Exception {
-				dispatcher.dispatchCall();
-				return true;
-			}
-		};
-	}
-	
-	/***
-	 * Prueba creada porque se deben cubrir los flujos excepcionales de la aplicacion
-	 * y este caso es una caso excepcional
-	 * 
-	 * @throws InterruptedException
-	 * @throws ExecutionException
-	 */
-	@Test
-	public void dispatchCallMasDiezLlamadas() throws InterruptedException, ExecutionException {
-		List<Callable<Boolean>> tasks = Collections.nCopies(11, calleable());
-		ExecutorService executorService = Executors.newFixedThreadPool(11);
-		List<Future<Boolean>> invokeAll = executorService.invokeAll(tasks);
-		Thread.sleep(15000);
-		
-		Assert.assertEquals(11, invokeAll.size());
-	}
-	
-	@Test
-	public void dispatchCallMenosDiezLlamadas() throws InterruptedException, ExecutionException {
-		List<Callable<Boolean>> tasks = Collections.nCopies(5, calleable());
-		ExecutorService executorService = Executors.newFixedThreadPool(10);
-		List<Future<Boolean>> invokeAll = executorService.invokeAll(tasks);
-		Thread.sleep(15000);
-		
-		Assert.assertEquals(5, invokeAll.size());
-	}
-	
+public class DispatcherTest {
+
+    private Dispatcher dispatcher = new Dispatcher();
+
+    /***
+     * Prueba Unitaria para cumplir con el requerimiento
+     * 		-> Procesar 10 llamadas al mismo tiempo
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Test
+    public void dispatchCallDiezLlamadas() throws InterruptedException, ExecutionException {
+        List<Callable<Boolean>> tasks = Collections.nCopies(10, calleable());
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<Boolean>> llamadaEjecutas = executorService.invokeAll(tasks);
+        Thread.sleep(15000);
+        Assert.assertEquals(10, llamadaEjecutas.size());
+    }
+
+    private Callable<Boolean> calleable() {
+        return new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                dispatcher.dispatchCall();
+                return true;
+            }
+        };
+    }
+
+    /***
+     * Prueba creada porque se deben cubrir los flujos excepcionales de la aplicacion
+     * y este caso es una caso excepcional
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Test
+    public void dispatchCallMasDiezLlamadas() throws InterruptedException, ExecutionException {
+        List<Callable<Boolean>> tasks = Collections.nCopies(11, calleable());
+        ExecutorService executorService = Executors.newFixedThreadPool(11);
+        List<Future<Boolean>> invokeAll = executorService.invokeAll(tasks);
+        Thread.sleep(15000);
+
+        Assert.assertEquals(11, invokeAll.size());
+    }
+
+    @Test
+    public void dispatchCallMenosDiezLlamadas() throws InterruptedException, ExecutionException {
+        List<Callable<Boolean>> tasks = Collections.nCopies(5, calleable());
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<Boolean>> invokeAll = executorService.invokeAll(tasks);
+        Thread.sleep(15000);
+
+        Assert.assertEquals(5, invokeAll.size());
+    }
+
 }
